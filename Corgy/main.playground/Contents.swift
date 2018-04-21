a8f2ae8666c031630c356df15b645ede33c58d79
Buqian Zheng <buqianz@andrew.cmu.edu>
2018-04-21 16:52:04 -0400

    Revert code in playground

--- Corgy/main.playground/Contents.swift
+++ Corgy/main.playground/Contents.swift
@@ -3,25 +3,16 @@ import Corgy
 
 var str = "Hello, playground"
 
-//let kernelWeight = Variable(1, 2, 2, 2)
-//for i in 0..<kernelWeight.value.count {
-//    kernelWeight.value[i] = Float(i)
-//}
-//
-//let conv2d = CPU.Conv2D(inChannels: 1, outChannels: 2, kernelSize: 2, weight: kernelWeight)
-//
-//let input = Variable(1, 1, 4, 4)
-//for i in 0..<input.value.count {
-//    input.value[i] = Float(i)
-//}
-//print(conv2d(input))
+let kernelWeight = Variable(1, 2, 2, 2)
+for i in 0..<kernelWeight.value.count {
+    kernelWeight.value[i] = Float(i)
+}
 
-let nn = NeuralNetwork()
+let conv2d = CPU.Conv2D(inChannels: 1, outChannels: 2, kernelSize: 2, weight: kernelWeight)
 
-let neg = Corgy.Neg(network: nn)
-
-let input = Variable(3,4)
+let input = Variable(1, 1, 4, 4)
 for i in 0..<input.value.count {
     input.value[i] = Float(i)
 }
-print(neg(input))
+print(conv2d(input))
+
