bf9c5b50f7051c7b07942816ccdfc208a8b2ff42
MrDwZ <hyk68691@hotmail.com>
2018-04-22 23:32:06 -0400

    Add trained mnist model from PyTorch Tutorial, loss is less than 1%.

---
+++ mnist/load.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.fc = nn.Linear(7 * 7 * 32, 10)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+if __name__ == "__main__":
+    model = CNN()
+    model.load_state_dict(torch.load('cnn.pkl'))
+    print(model.state_dict)
+    print(model.state_dict())
