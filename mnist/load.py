9db30133fa08759b5be8eeeca68e795af3ddd5d4
MrDwZ <hyk68691@hotmail.com>
2018-04-23 00:03:50 -0400

    Add simplified model.

--- mnist/load.py
+++ mnist/load.py
@@ -6,13 +6,11 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, padding=2),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(1, 16, kernel_size=5),
             nn.ReLU(),
             nn.MaxPool2d(2))
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, padding=2),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(16, 32, kernel_size=5),
             nn.ReLU(),
             nn.MaxPool2d(2))
         self.fc = nn.Linear(7 * 7 * 32, 10)
--- mnist/main.py
+++ mnist/main.py
@@ -35,12 +35,10 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=5, padding=2),
-            nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(2))
         self.layer2 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=5, padding=2),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2))
         self.fc = nn.Linear(7*7*32, 10)
