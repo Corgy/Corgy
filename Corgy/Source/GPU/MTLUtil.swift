0861ca3af11ffc7352930663bac1353fb2fc55a5
MrDwZ <hyk68691@hotmail.com>
2018-04-26 15:48:48 -0400

    Improve access control of CorgyResource.

--- Corgy/Source/GPU/MTLUtil.swift
+++ Corgy/Source/GPU/MTLUtil.swift
@@ -19,38 +19,5 @@ struct TEMP_PARAM {
 ///
 /// - parameter input: Variables in input will be set to buffer of encoder
 ///             in order(from 1). Last one will be used as output.
-func submitWork(_ resource: CorgyResource,
-                       name function: String,
-                       in input: Variable...,
-                       param: TEMP_PARAM? = nil) {
-    let output = input[input.count-1]
-    // 坑: must create new command buffer and encoder for each compute task.
-    let commandBuffer = resource.commandQueue.makeCommandBuffer()!
-    let f = resource.library.makeFunction(name: function)!
-    let encoder = commandBuffer.makeComputeCommandEncoder()!
-    let pipelineState = try! resource.device.makeComputePipelineState(function: f)
-    encoder.setComputePipelineState(pipelineState)
-    
-    var outputBuffer: MTLBuffer?, outputLength: Int?
-    for (i, variable) in input.enumerated() {
-        let length = variable.value.count * MemoryLayout<Variable.DataType>.stride
-        // TODO: use bytes no copy, which might cause a large amount of refractor
-        let buf = resource.device.makeBuffer(bytes: variable.value, length: length, options: [])
-        encoder.setBuffer(buf, offset: 0, index: i)
-        (outputBuffer, outputLength) = (buf, length)
-    }
-    
-    let threadsPerThreadGroup = MTLSizeMake(min(THREAD_PER_GROUP, output.value.count), 1, 1)
-    let threadGroups = MTLSizeMake((output.value.count + THREAD_PER_GROUP - 1) / THREAD_PER_GROUP, 1, 1)
-    
-    encoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadsPerThreadGroup)
-    
-    encoder.endEncoding()
-    commandBuffer.commit()
-    commandBuffer.waitUntilCompleted()
-    
-    // TODO: after makeBuffer(bytesNoCopy) is used, following code can be get rid of
-    let content = NSData(bytesNoCopy: outputBuffer!.contents(), length: outputLength!, freeWhenDone: false)
-    content.getBytes(&output.value, length: outputLength!)
-}
+
 
